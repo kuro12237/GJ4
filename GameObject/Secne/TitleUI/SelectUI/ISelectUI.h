66acc93354309cba75acf77397def3cb54b76649
kt610 <kt610@MSI>
2025-09-07 21:18:35 +0900

    セレクトUI追加途中

--- GameObject/Secne/TitleUI/SelectUI/ISelectUI.h
+++ GameObject/Secne/TitleUI/SelectUI/ISelectUI.h
@@ -1,1 +1,35 @@
 #pragma once
+#include "CLEYERA/CLEYERA.h"
+
+class ISelectUI : public SpriteComponent
+{
+public:
+	ISelectUI() {};
+	virtual ~ISelectUI() {};
+
+	virtual void Init() {}
+
+	virtual void Update() {}
+
+#pragma region Set
+
+	virtual void SetScale(const Math::Vector::Vec2& scale) {
+		this->scale_ = {
+			scale.x, scale.y, 0.0f
+		};
+	}
+
+	virtual void SetTranslate(const Math::Vector::Vec2& pos) {
+		this->translate_ = {
+			pos.x, pos.y, 0.0f
+		};
+	}
+
+
+#pragma endregion
+
+
+private:
+
+
+};
